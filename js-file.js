258bd9ebd8b5b5343e4059a03dea358d5cbcfb2e
Aidan Brook <aidanzhaobrook@gmail.com>
2022-05-29 18:10:50 -0400

    Create Math Operation Functions
    Addition, Subtraction, Multiplication, and Division were created using
    the functions add, subtract, multiply, and divide. These are the basic
    operations that a calculator must do.

---
+++ js-file.js
@@ -0,0 +1,19 @@
+//Add two number inputs
+function add(a, b) {
+  return a + b;
+}
+
+//Subtract two number inputs
+function subtract(a, b) {
+  return a - b;
+}
+
+//Multiply two number inputs
+function multiply(a, b) {
+  return a * b;
+}
+
+//Divide two number inputs
+function divide(a, b) {
+  return a / b;
+}
