c66dc1d036d288b3f009b4500f0dd8d6d7222953
Aidan Brook <aidanzhaobrook@gmail.com>
2022-05-29 18:17:16 -0400

    Add Function That Calls Math Operation
    Based on what input operator (+,-,*,or/) is given the operate function
    calls the appropriate calculation function.

--- js-file.js
+++ js-file.js
@@ -17,3 +17,16 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+//Uses one of the calculation functions based on the input operator
+function operate(operator, a, b) {
+  if (operator == "+") {
+    add(a, b);
+  } else if (operator == "-") {
+    subtract(a, b);
+  } else if (operator == "*") {
+    multiply(a, b);
+  } else if (operator == "/") {
+    divide(a, b);
+  }
+}
